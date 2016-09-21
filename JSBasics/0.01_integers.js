0d80cd1977c6aa044758f4381d3422ec593056e7
Danielle <mrshayesworks@gmail.com>
2016-09-21 10:34:49 -0400

    added store project and project and javascript

---
+++ JSBasics/0.01_integers.js
@@ -0,0 +1,38 @@
+var num = 7;
+var newNum = 5; //this is camel casing
+var newer_num = 10; // this is snake casing
+
+num2 = 6;
+
+console.log(5+5);
+
+//console.log prints to the console
+
+// console.log(10-5)
+// console.log(7*5)
+// console.log(200/4)
+
+//Modulus operator
+
+// console.log(10 % 5);
+// console.log(4 % 3);
+
+// console.log(13 % 6);
+
+// var	numOp = 25 + 100;
+// console.log(numOp);
+
+var newNum = 10
+newNum = newNum +1;
+newNum++;
+console.log(newNum)
+newNum = newNum - 1;
+console.log(newNum);
+
+newNum += 10;
+console.log(newNum);
+
+newNum =newNum - 10;
+console.log(newNum);
+
+console.log(newNum);
